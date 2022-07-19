2de8dc2fd4feeaadc6dd7c10d4423c22740c3ae5
Jan Specker <jan.specker@posteo.de>
2022-07-19 14:40:40 +0200

    Add helper functions to run.sh

--- run.sh
+++ run.sh
@@ -1,27 +1,52 @@
 #!/bin/bash
+
+
 set -eou pipefail
-# set -x
 
 
 if [[ $# -lt 1 ]]; then
-  echo "Usage: $0 format | lint" >&2
+  echo "Usage: $0 create-env | format | lint | test | update | ci" >&2
   exit 1
 fi
 
+task_create_env() {
+    poetry install
+}
 
 task_format_python() {
-  black python/*
+    task_create_env
+    poetry run black python/*
 }
 
 
 task_lint() {
-  shellcheck ./*.sh
-  pylint python/*
+    task_create_env
+    shellcheck ./*.sh
+    poetry run pylint python/*
+}
+
+task_test() {
+    task_create_env
+    (cd python && poetry run py.test)
+}
+
+task_ci() {
+    task_format_python
+    task_lint
+    task_test
+}
+
+task_update_dependencies() {
+    poetry update
 }
 
 
 cmd=$1
 case "$cmd" in
+  create-env) task_create_env ;;
   format) task_format_python ;;
   lint) task_lint ;;
+  test) task_test ;;
+  update) task_update_dependencies ;;
+  ci) task_ci ;;
 esac
