18308287d1a0c30dbafd7d25a10a0b6c3ac0584f
Jan Specker <jan.specker@posteo.de>
2022-07-19 14:40:40 +0200

    Use vimwiki filename for title

--- vimwiki_extensions/vimwiki_template_jira_story.py
+++ vimwiki_extensions/vimwiki_template_jira_story.py
@@ -1,5 +1,7 @@
 #!/home/jan/miniconda3/bin/python
 
+import sys
+
 """
 When run, this module prints out a template for a Jira story entry.
 
@@ -23,10 +25,10 @@ TEMPLATE = """# {jira_story_tag}
 ## Questions"""
 
 
-def get_jira_tag():
+def get_vimwiki_filename():
     """Reads filename and returns it. Used here to get name of Jira Tag."""
-    return "PIT-XXXX"  # TODO: Return real jira tag
+    return sys.argv[1].split("/")[-1].split(".")[0]
 
 
 if __name__ == "__main__":
-    print(TEMPLATE.format(jira_story_tag=get_jira_tag()))
+    print(TEMPLATE.format(jira_story_tag=get_vimwiki_filename()))
